f312a914b5b58fe593d3d35e348765000b38dc4b
Mick <micksmits96@gmail.com>
2025-11-22 18:17:58 +0100

    feat: add content, dark mode and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,51 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="m-14 font-mono">
+      <div className="text-sm leading-none font-medium font-mono">
+        <Link
+          href="https://ironclave.com"
+          className="text-accent-foreground underline underline-offset-4"
+        >
+          Ironclave
+        </Link>{" "}
+        /{" "}
+        <Link
+          href="https://x.com/micksmits"
+          className="text-accent-foreground underline underline-offset-4"
+          target="_blank"
+        >
+          Twitter
+        </Link>{" "}
+        /{" "}
+        <Link
+          href="https://github.com/ironclave"
+          className="text-accent-foreground underline underline-offset-4"
+          target="_blank"
+        >
+          GitHub
+        </Link>
+      </div>
+      <div className="text-md leading-6 font-medium mt-8 max-w-2xl">
+        Ironclave builds products that look good, feel good, and tackle real
+        problems. We care about detail and creating experiences people trust.
+        The goal is simple: craft beautiful apps that solve something real and
+        raise standards.
+      </div>
+      <div className="max-w-sm mt-8">
+        <h4 className="scroll-m-20 text-lg font-medium tracking-tight">
+          Bizdex
+        </h4>
+        <Link
+          href="https://bizdex.app"
+          className="text-accent-foreground text-sm underline underline-offset-4"
+          target="_blank"
+        >
+          Bizdex finds you people to do business with and lets you manage your
+          network efficiently.
+        </Link>
+      </div>
     </div>
   );
 }
